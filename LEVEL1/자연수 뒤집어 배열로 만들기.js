78641e66a3ed9ed4be0d12b32bf39d039c8588bd
HyeWon Kim <76997276+haeburney@users.noreply.github.com>
2022-10-21 16:31:35 +0900

    Update 20221021

--- LEVEL1/자연수 뒤집어 배열로 만들기.js
+++ LEVEL1/자연수 뒤집어 배열로 만들기.js
@@ -1,4 +1,4 @@
-// 풀이 1 
+// 풀이 1 -> 배열을 반으로 나눠서 음료수 나눠 담기로 
 function solution(n) {
     var answer = (n + '').split('');
     var i, end = answer.length - 1;
@@ -13,4 +13,22 @@ function solution(n) {
     return answer;  
 }
 
-// 풀이 2 
+
+// 풀이 2 -> 10씩 나머지를 배열에 push 하는 방법 
+function solution(n) {
+    var answer = [];
+    while(n>0){
+        answer.push(n%10);
+        n = parseInt(n / 10);
+    }
+    
+    return answer;
+}
+
+
+// 풀이 3 -> 문자로 잘라서 다시 parseInt로 만들어서 reverse 하기.
+function solution(n) {
+    var answer = (n + '').split('').reverse().map(o => parseInt (o));
+    
+    return answer;
+}
