2675287ee489f91fa78328eb54914b6d114ec55d
Kim Hye Won <76997276+haeburney@users.noreply.github.com>
2022-10-21 19:32:49 +0900

    20221021

---
+++ LEVEL1/나머지가 1이 되는 수 찾기.js
@@ -0,0 +1,13 @@
+function solution(n) {
+    var num = 1;
+    
+    while(num++){
+        if(n % num === 1){
+            return num;
+           // break;
+        }
+    }
+}
+
+
+// break 생략해도 가능하다.
