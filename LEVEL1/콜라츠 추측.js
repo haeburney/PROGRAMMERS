19e73b3e77ec0aaba0574e3c5db87bd2519b25cc
Kim Hye Won <76997276+haeburney@users.noreply.github.com>
2022-10-22 21:07:15 +0900

    Update 20221022

--- LEVEL1/콜라츠 추측.js
+++ LEVEL1/콜라츠 추측.js
@@ -15,3 +15,16 @@ function solution(num) {
     
     return -1;
 }
+
+
+// 좀 더 간결한 코드가 있었다
+function solution(num) {
+    var answer = 0;
+    
+    while(answer!=500 && num!==1){
+        (num % 2 ===0) ? num /= 2 :   num = 3 * num + 1;
+        answer++;
+    }
+    
+    return (num ===1 ) ? answer : -1;
+}
