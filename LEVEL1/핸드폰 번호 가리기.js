518211cefe3c0f1fd4487a58c96847dca125611e
Kim Hye Won <76997276+haeburney@users.noreply.github.com>
2022-10-23 22:14:30 +0900

    Update 20221023

--- LEVEL1/핸드폰 번호 가리기.js
+++ LEVEL1/핸드폰 번호 가리기.js
@@ -12,3 +12,12 @@ function solution(phone_number) {
     
     return answer;
 }
+
+
+// 더 간단한 풀이 repeat, slice 
+
+function solution(phone_number) {
+    var answer = '*'.repeat(phone_number.length-4)+ phone_number.slice(-4);
+    
+    return answer;
+}
