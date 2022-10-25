bb79ada6eff316d19c97909d8aef8c7fb68503d4
Kim Hye Won <76997276+haeburney@users.noreply.github.com>
2022-10-25 23:42:37 +0900

    20221025

---
+++ LEVEL1/약수의 개수와 덧셈.js
@@ -0,0 +1,9 @@
+function solution(left, right) {
+    var answer = 0;
+    
+    for(var i = left; i <= right; i++){
+        (Number.isInteger(Math.sqrt(i)))? answer -= i : answer += i;
+    }
+    
+    return answer;
+}
