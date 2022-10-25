73da50b40225a45b378e6a36ce935aa53a9f5a49
Kim Hye Won <76997276+haeburney@users.noreply.github.com>
2022-10-26 01:16:46 +0900

    Update 20221026

--- LEVEL2/최댓값과 최솟값.js
+++ LEVEL2/최댓값과 최솟값.js
@@ -6,3 +6,12 @@ function solution(s) {
     
     return answer = Math.min.apply(Math, array) + " " + Math.max.apply(Math, array) ;
 }
+
+
+// 이렇게도 간응한 자바스크립트 대박.. 밑이 훨 빠름
+function solution(s) {
+    var array = s.split(" ");
+    
+    return Math.min.apply(Math, array) + " " + Math.max.apply(Math, array) ;
+}
+
