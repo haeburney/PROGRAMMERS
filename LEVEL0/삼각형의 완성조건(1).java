a0ac19ba953d2358a076c526965be41ab4036f56
Kim Hye Won <76997276+haeburney@users.noreply.github.com>
2023-08-21 14:16:37 +0900

    Create 삼각형의 완성조건(1).java

---
+++ LEVEL0/삼각형의 완성조건(1).java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+
+class Solution {
+    public int solution(int[] sides) {
+        Arrays.sort(sides);
+        
+        if(sides[2] < sides[1] + sides[0]){
+            return 1;
+        } else {
+            return 2;
+        }
+    }
+}
